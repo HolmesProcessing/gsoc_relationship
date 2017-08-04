80dfb35180b9f0e3168af4da573f0e4ae337de9a
T0day <zhang_juntao@sjtu.edu.cn>
2017-08-04 21:32:14 +0800

    Renew some bugs about peinfo
    I forget to save the int features of peinfo.
    So I concat the int features and binary features of peinfo and then
    save them.

--- ml/pre-processing/get_VT_signatures.scala
+++ ml/pre-processing/get_VT_signatures.scala
@@ -26,7 +26,7 @@ val VT_results_by_sha256_rdd = VT_results_by_sha256_meta.keyBy(x => (x.sha256,x.
 val VT_join_results = VT_results_by_service_name_rdd.join(VT_results_by_sha256_rdd).map(x => (new VT_join_results_class(x._1._1,x._1._2, unzip(x._2._2.results)))).distinct().cache()
 val sample_signatures_rdd = VT_join_results.flatMap(x=>Json.parse(x.results) \ "scans" \\ "result").map(x=>Json.stringify(x)).filter( x=> !(x == "null"))
 val sample_signatures_split_rdd = sample_signatures_rdd.flatMap(x=>x.replaceAll("""["]""","").replaceAll("""\![a-zA-Z0-9\s\+]+""","").replaceAll("""@[a-zA-Z0-9\s\+]+""","").replaceAll("""~[a-zA-Z0-9\s\+]+""","").replaceAll("""[\(|\[|{][a-zA-Z0-9\s\+]*[\)|\]|}]""","").replaceAll("""(\.|\!|\:|\_|\-|\\|/|\[|\])"""," ").split(" ")).filter(x=>(x.size>3)).filter(x=>deleteNumberInSampleSignatures(x)).map(x=>x.toLowerCase())
-val signatures_prefix_rdd = sc.textFile("./t0day/prefix.txt").map(x=>x.toLowerCase())
+val signatures_prefix_rdd = sc.textFile("./prefix.txt").map(x=>x.toLowerCase())
 val family_signatures_subtract_rdd = sample_signatures_split_rdd.subtract(signatures_prefix_rdd)
 val family_signatures_sorted_rdd = sc.parallelize(family_signatures_subtract_rdd.countByValue().toSeq).filter(x=>(x._2>50)).sortBy(x=>x._2,false)
 val family_signatures_list = family_signatures_sorted_rdd.keys.collect().toList
@@ -42,4 +42,4 @@ def findAllInFamilySignatures(sample_signatures_seq : Seq[String]) : Array[Doubl
 }
 
 val VT_sample_signatures_final_array_rdd = VT_sample_signatures_initial_seq_rdd.map(x=>new VT_sample_signatures_final_array_rdd_class(x.sha256,findAllInFamilySignatures(x.seq_results)))
-VT_sample_signatures_final_array_rdd.toDF().write.format("parquet").save("./t0day/VT_sample_signatures_final_array.parquet")
+VT_sample_signatures_final_array_rdd.toDF().write.format("parquet").save("./VT_sample_signatures_final_array.parquet")
